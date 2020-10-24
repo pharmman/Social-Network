923764a0a4140ece7de542df089bbea8ff80bca7
Pharmman <pharm.sale777@gmail.com>
2020-10-24 23:36:05 +0300

    Added store, need fix it

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -32,45 +32,50 @@ export type StateType = {
     profilePage: ProfilePageType
     dialogsPage: DialogsPageType
 }
+type StoreType = {
+    _state: StateType
+    changingValueForNewPost: (value:string) => void
+    addPost: () => void
+}
 
-export let state: StateType = {
-    profilePage: {
-        messageForNewPost: '',
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 12},
-            {id: 3, message: 'Yo Yo Yo', likesCount: 12},
-            {id: 4, message: 'It\'s revolution Jhonny!', likesCount: 100500}
-        ]
+const store:StoreType = {
+    _state:{
+        profilePage: {
+            messageForNewPost: '',
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 12},
+                {id: 3, message: 'Yo Yo Yo', likesCount: 12},
+                {id: 4, message: 'It\'s revolution Jhonny!', likesCount: 100500}
+            ]
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Steve'},
+                {id: 2, name: 'Tim'},
+                {id: 3, name: 'Jhonny'},
+                {id: 4, name: 'Andy'},
+                {id: 5, name: 'Sasha'},
+            ],
+            messages: [
+                {id: 1, message: 'Hello'},
+                {id: 2, message: 'How is your day?'},
+                {id: 3, message: 'Introduce Iphone'},
+                {id: 4, message: 'Introduce Ipad'},
+                {id: 5, message: 'Google suck'},
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Steve'},
-            {id: 2, name: 'Tim'},
-            {id: 3, name: 'Jhonny'},
-            {id: 4, name: 'Andy'},
-            {id: 5, name: 'Sasha'},
-        ],
-        messages: [
-            {id: 1, message: 'Hello'},
-            {id: 2, message: 'How is your day?'},
-            {id: 3, message: 'Introduce Iphone'},
-            {id: 4, message: 'Introduce Ipad'},
-            {id: 5, message: 'Google suck'},
-        ]
+    changingValueForNewPost(value: string) {
+        this._state.profilePage.messageForNewPost = value;
+        rerenderEntireTree()
+    },
+    addPost() {
+        const newPost: PostsDataType = {id: 5, message: this._state.profilePage.messageForNewPost, likesCount: 0};
+        this._state.profilePage.posts.push(newPost);
+        store.changingValueForNewPost('');
+        rerenderEntireTree()
     }
-}
-
-export function changingValueForNewPost(value: string) {
-    state.profilePage.messageForNewPost = value;
-    rerenderEntireTree()
-}
-
-export function addPost() {
-    const newPost: PostsDataType = {id: 5, message: state.profilePage.messageForNewPost, likesCount: 0};
-    state.profilePage.posts.push(newPost);
-    changingValueForNewPost('');
-    rerenderEntireTree()
 }
 
 export const subscribe = (observer:() => void) => {
