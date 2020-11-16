41bdef34ce8be229e2e454b8f685fc2345309c0a
Pharmman <pharm.sale777@gmail.com>
2020-11-17 01:51:13 +0300

    fixed userAvatar

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {UserType} from '../../redux/users-reducer';
 import classes from './Users.module.css'
 import  axios from 'axios'
+import userAvatar from '../../assets/images/userAvatar.jpg'
 
 
 type UserPropsType = {
@@ -36,7 +37,7 @@ export const Users = (props: UserPropsType) => {
         {props.users.map(u => <div key={u.id}>
                 <div className={classes.wrapper}>
                     <div className={classes.avatar__wrapper}>
-                        <img alt={'Avatar'} className={classes.avatar} src={'https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png'}/>
+                        <img alt={'Avatar'} className={classes.avatar} src={u.photos.small !== null? u.photos.small : userAvatar}/>
                         <div>
                             {u.followed ? <button onClick={() => props.unFollow(u.id)}>Unfollowed</button> :
                                 <button onClick={() => props.follow(u.id)}>Follow</button>}
